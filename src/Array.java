57a4a07597ab8fe5ef9dfca6581a5471b504bd57
Manan <manannagpal003@gmail.com>
2025-09-29 14:55:13 +0530

    adding practice set1

---
+++ src/Array.java
@@ -0,0 +1,17 @@
+public class Array {
+    public static void main(String[] args) {
+      //  int marks[] = new int[3];
+        int marks[]={97,98,95};
+//        marks[0] = 79;
+//        marks[1] = 89;
+//        marks[2] = 91;
+
+
+        //  System.out.println(marks[0]);
+        //  System.out.println(marks[1]);
+        //  System.out.println(marks[2]);
+        for (int i = 0; i < 3; i++) {
+            System.out.println(marks[i]);
+        }
+    }
+}
